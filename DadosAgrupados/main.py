46885df6e86f9be1edd1bbdc9023f5de63ab9df8
AndradeMatheus <matheus.andrade.santos@gmail.com>
2019-10-29 23:52:40 -0300

    Corrigido comportamento de cálculos para funcionar com o dataframe.

--- DadosAgrupados/main.py
+++ DadosAgrupados/main.py
@@ -1,29 +1,29 @@
 import math
 import pandas as pd
 
-df = pd.DataFrame(columns=["Valor", "Quantidade"])
+df = pd.DataFrame(columns=["valor", "quantidade"])
 n = int(input("Entre com o número de agrupamentos: "))
 
 for _ in range(n):
     vl = input("Entre com o valor: ")
-    qt = input("Entre com a quantidade de valores para {}: ".format(vl))
-    df1 = pd.DataFrame(data=[[vl,qt]],columns=["Valor", "Quantidade"])
+    qt = (int(input("Entre com a quantidade de valores para {}: ".format(vl))))
+    df1 = pd.DataFrame(data=[[vl,qt]],columns=["valor", "quantidade"])
     df = pd.concat([df,df1], axis=0)
 
 df.index = range(len(df.index))
 print(df)
 
-desvioporgrupo = data.groupby(["col1"]).agg({"col2": ["std"]})
+desvioporgrupo = df.groupby(["valor"]).agg({"quantidade": ["std"]})
 print('Desvio por grupo: \n', desvioporgrupo)
 
-desvio = data.water_need.std()
+desvio = df.quantidade.std()
 print('\nDesvio: ', format(desvio))
 
-media = data.water_need.mean()
+media = df.quantidade.mean()
 print('Media: ', format(media))
 
-dispersao = data.water_need.skew()
+dispersao = df.quantidade.skew()
 print('Dispersao: ', dispersao)
 
-soma = data.water_need.sum()
+soma = df.quantidade.sum()
 print('Soma: ', soma)
