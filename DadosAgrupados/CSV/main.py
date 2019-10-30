1f6b718742a42d9bf92a4fb116befcb65d72dcfb
AndradeMatheus <matheus.andrade.santos@gmail.com>
2019-10-29 22:37:33 -0300

    Corrigido comportamento da solucao CSV.

--- DadosAgrupados/CSV/main.py
+++ DadosAgrupados/CSV/main.py
@@ -2,17 +2,17 @@ import math
 import pandas as pd
 
 pd.options.display.max_rows = 10
-data  = pd.read_csv("data.csv")
+data  = pd.read_csv("untitled.csv")
 print('Dados do CSV: \n', data)
 
 desvioporgrupo = data.groupby(["animal"]).agg({"water_need": ["std"]})
 print('Desvio por grupo: \n', desvioporgrupo)
 
 desvio = data.water_need.std()
-print(format('Desvio: ', desvio))
+print('\nDesvio: ', format(desvio))
 
 media = data.water_need.mean()
-print(format('Media: ', media))
+print('Media: ', format(media))
 
 dispersao = data.water_need.skew()
 print('Dispersao: ', dispersao)
